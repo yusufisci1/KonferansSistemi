bf50a906e61ec45cf119b94a856ccd257d51496f
yusufisci1 <yusuf_isci@windowslive.com>
2025-03-16 17:46:20 +0300

    yeni özellikler eklendi

--- server.js
+++ server.js
@@ -25,10 +25,15 @@ let isMeetingStarted = false;  //Toplantı
 let isAttendanceStarted = false; //Yoklama
 let isVotingStarted = false; //Oylama
 
+let hasRequestedToSpeak = {}; // Söz isteyen kullanıcıları takip et
+let hasJoinedAttendance = {}; // Yoklamaya katılanları takip et
+let hasJoinedMeeting = {}; // Toplantıya katılanları takip et
+let hasVoted = {};
+
 io.on("connection", (socket) => {
     console.log("Bir kullanıcı bağlandı.");
 
-    // Yeni bağlanan kullanıcıya toplantının açık olup olmadığını bildir
+    // Yeni bağlanan kullanıcıya mevcut oturum durumlarını bildir
     if (isMeetingStarted) {
         socket.emit("meetingStarted");
     }
@@ -45,54 +50,81 @@ io.on("connection", (socket) => {
         isMeetingStarted = true;
     });
 
-    // (Diğer event listener'lar burada kalacak)
-
-   
     socket.on("startAttendance", () => {
-        io.emit("attendanceStarted");
         console.log("Yoklama başladı!");
+        io.emit("attendanceStarted");
         isAttendanceStarted = true;
     });
 
     socket.on("startVoting", () => {
-        
-        io.emit("votingStarted");
         console.log("Oylama başladı!");
+        io.emit("votingStarted");
         isVotingStarted = true;
     });
 
+    // Söz isteme
     socket.on("requestToSpeak", (username) => {
         if (!isMeetingStarted) {
             socket.emit("errorMessage", "Toplantı başlamadı!");
             return;
         }
+        if (hasRequestedToSpeak[username]) {
+            socket.emit("errorMessage", "Zaten söz istediniz, bekleyin!");
+            return;
+        }
+        hasRequestedToSpeak[username] = true; // Kullanıcıya söz hakkı isteme kaydı ekle
         console.log(`${username} söz istedi.`);
         io.emit("newRequest", { username });
     });
 
-    socket.on("vote", (username, choice) => {
-        if (!isVotingStarted) {
-            socket.emit("errorMessage", "Oylama başlamadı!");
-            return;
-        }
-        console.log(`${username} oylamada ${choice} dedi.`);
-        io.emit("voteResult", { username, choice });
-    });
-
+    // Yoklama katılımı
     socket.on("attendance", (username) => {
         if (!isAttendanceStarted) {
             socket.emit("errorMessage", "Yoklama başlamadı!");
             return;
         }
+        if (hasJoinedAttendance[username]) {
+            socket.emit("errorMessage", "Zaten yoklamaya katıldınız!");
+            return;
+        }
+        hasJoinedAttendance[username] = true;
         console.log(`${username} yoklamaya katıldı.`);
         io.emit("attendanceMarked", { username });
-
     });
 
+    // Toplantıya katılım
+    socket.on("joinMeeting", (username) => {
+        if (!isMeetingStarted) {
+            socket.emit("errorMessage", "Toplantı başlamadı!");
+            return;
+        }
+        if (hasJoinedMeeting[username]) {
+            socket.emit("errorMessage", "Zaten toplantıya katıldınız!");
+            return;
+        }
+        hasJoinedMeeting[username] = true;
+        console.log(`${username} toplantıya katıldı.`);
+        io.emit("meetingJoined", { username });
+    });
+
+    // Oylama
+    socket.on("vote", (username, choice) => {
+        if (!isVotingStarted) {
+            socket.emit("errorMessage", "Oylama başlamadı!");
+            return;
+        }
+        if (hasVoted[username]) {
+            socket.emit("errorMessage", "Zaten oy kullandınız!");
+            return;
+        }
+        hasVoted[username] = true;
+        console.log(`${username} oylamada ${choice} dedi.`);
+        io.emit("voteResult", { username, choice });
+    });
 
     socket.on("approveRequest", (username) => {
-    console.log(`${username} söz hakkı onaylandı.`);
-    io.emit("requestApproved", { username });
+        console.log(`${username} söz hakkı onaylandı.`);
+        io.emit("requestApproved", { username });
     });
 
     socket.on("disconnect", () => {
